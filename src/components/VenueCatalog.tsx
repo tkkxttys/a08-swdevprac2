468dbcc9f54777ab932f341d390163fd8c77c8cf
tkkxttys <kittycatty.2548@gmail.com>
2025-03-16 03:16:32 +0700

    edit tc 1

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -1,4 +1,4 @@
-'use client'
+/*'use client'
 import ProductCard from "./Card"
 import Link from "next/link"
 
@@ -20,4 +20,24 @@ export default async function VenueCatalog({venueJson}: {venueJson: Promise<Venu
             </div>
         </>
     )
+}*/
+
+import ProductCard from "./Card";
+import Link from "next/link";
+
+export default async function VenueCatalog({ venueJson }: { venueJson: VenueJson }) {  
+    return (
+        <>
+            Explore {venueJson.count} models in our catalog
+            <div style={{ margin: "20px", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around", alignContent: "space-around" }}>
+                {
+                    venueJson.data.map((venueItem) => (
+                        <Link key={venueItem.id} href={`/venue/${venueItem.id}`} className="w-1/5">
+                            <ProductCard venueName={venueItem.name} imgSrc={venueItem.picture} />
+                        </Link>
+                    ))
+                }
+            </div>
+        </>
+    );
 }
