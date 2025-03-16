a4fb0da300c0130da5d86e1282863a73602b0b32
tkkxttys <kittycatty.2548@gmail.com>
2025-03-16 13:52:55 +0700

    edit jest

--- src/libs/getVenues.tsx
+++ src/libs/getVenues.tsx
@@ -4,7 +4,7 @@ export default async function getVenues() {
 
     const response = await fetch("https://a08-venue-explorer-backend.vercel.app/api/v1/venues")
     if(!response.ok) {
-        throw new Error("Failed to fetch cars")
+        throw new Error("Failed to fetch venues")
     }
 
     return await response.json()
